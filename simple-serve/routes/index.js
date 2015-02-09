7b49678cd1cd3e41c2dae0f1a1a5386f68ae6b29
ntuvera <ntuvera@gmail.com>
2015-02-09 01:44:15 -0500

    simple-serve -- with commented options for res

--- simple-serve/routes/index.js
+++ simple-serve/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET Status Codes Only. */
+// router.get('/', function(req, res) {
+//   res.send(200);
+// });
 
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', {title: 'Simple Serve via Express' });
+// });
+
+/* GET JSON object. */
+// router.get('/', function(req, res) {
+//   // res.render('index', { title: 'Simple Serve via Express'});
+//   res.send({
+//     users: ['Nate', 'Tuvera']
+//   });
+//
+
+/* GET console req params log & JSON name change. */
+// router.get('/', function(req, res) {
+//   console.log(req.query);
+//   if (req.query.title) {
+//     res.render('index', {title: req.query.title});
+//   } else {
+//   res.render('index', {title: 'Simple-Serve'});
+//   }
+// });
+
+/* GET console log Params log (res required to stop pending forever) */
+router.get('/users/:id', function(req, res){
+  console.log(req.params);
+  res.send(req.params.id, 200);
+})
 module.exports = router;
